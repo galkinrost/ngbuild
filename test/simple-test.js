ea3b55da2ee812a6aa3788077272870a4c58c6f3
galkinrost <galkinrost@gmail.com>
2014-04-07 19:43:36 +0400

    streaming api

--- test/simple-test.js
+++ test/simple-test.js
@@ -7,7 +7,7 @@ describe('Scripts concat', function () {
         var result = "angular.module('App.controllers', []);\n" +
             "angular.module('App', ['App.controllers']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/simple_app.js',
             dest: 'app/simple_app.build.js'
         }).on('end', function () {
@@ -28,7 +28,7 @@ describe('Scripts concat', function () {
             "angular.module('App.controllers').controller('AppSecondCtrl', function () {\n});\n" +
             "angular.module('App', ['App.controllers']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/controllers_app.js',
             dest: 'app/controllers_app.build.js'
         }).on('end', function () {
@@ -49,7 +49,7 @@ describe('Scripts concat', function () {
             "});\n" +
             "angular.module('App', ['App.directivesWithTemplate']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/template_directive_app.js',
             dest: 'app/template_directive_app.build.js'
         }).on('end', function () {
@@ -69,7 +69,7 @@ describe('Scripts concat', function () {
             "    return { template: '<style>.directive {\\n}</style>' };\n});\n" +
             "angular.module('App', ['App.directivesWithStyles']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/styles_directive_app.js',
             dest: 'app/styles_directive_app.build.js'
         }).on('end', function () {
@@ -89,7 +89,7 @@ describe('Scripts concat', function () {
             "    return { template: '<style>.directive {\\n}</style><span>templates/directives/template.html</span>' };\n});\n" +
             "angular.module('App', ['App.directivesWithTemplateAndStyles']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/template_and_styles_directive_app.js',
             dest: 'app/template_and_styles_directive_app.build.js'
         }).on('end', function () {
@@ -110,7 +110,7 @@ describe('Scripts concat', function () {
             "angular.module('App.controllers').controller('AppSecondCtrl', function () {\n});\n" +
             "angular.module('App', [\n    'NgRoute',\n    'App.controllers'\n]);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/full_app.js',
             dest: 'app/full_app.build.js'
         }).on('end', function () {
@@ -133,7 +133,7 @@ describe('Scripts concat', function () {
             "    return {};\n});\n" +
             "angular.module('App', [\n    'App.controllers',\n    'App.directivesWithControllers'\n]);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/dublicates_app.js',
             dest: 'app/dublicates_app.build.js'
         }).on('end', function () {
@@ -156,7 +156,7 @@ describe('Scripts concat', function () {
             "    return {};\n});\n" +
             "angular.module('App', ['App.directivesWithLib']);\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/lib_app.js',
             dest: 'app/lib_app.build.js'
         }).on('end', function () {
@@ -171,12 +171,12 @@ describe('Scripts concat', function () {
 
     });
 
-    it.only('Build app with routes', function (done) {
+    it('Build app with routes', function (done) {
         var result = "angular.module('App', ['ngRoute'], function ($routeProvider) {\n" +
             "    $routeProvider.when('/url/1', { template: '<style>.styles{\\n\\n}</style><span>templates/template.html</span>' }).when('/url/2', { template: '<style>.styles{\\n\\n}</style><span>templates/template.html</span>' });\n" +
             "});\n"
 
-        ngbuild({
+        ngbuild.build({
             src: 'app/router_app.js',
             dest: 'app/router_app.build.js'
         }).on('end', function () {
@@ -188,5 +188,36 @@ describe('Scripts concat', function () {
                 });
             }, 100);
         });
+    });
+
+    it.only('Building with custom writable stream', function (done) {
+        var content = fs.readFileSync('app/simple_app.js');
+        var writeResult = '';
+        var Writable = require('stream').Writable;
+        var CustomWritable = function () {
+            Writable.call(this);
+        }
+
+        require('util').inherits(CustomWritable, Writable);
+
+        CustomWritable.prototype._write = function (chunk, encoding, done) {
+            writeResult += chunk.toString();
+            done();
+        }
+
+        var result = "angular.module('App.controllers', []);\n" +
+            "angular.module('App', ['App.controllers']);\n"
+        var writable = new CustomWritable();
+
+        var readable = ngbuild.getReadable({
+            content: content,
+            src: 'app/simple_app.js'
+        })
+        readable.on('end', function () {
+            writeResult.should.be.equal(result);
+            done()
+        });
+        readable.pipe(writable);
+
     });
 });
